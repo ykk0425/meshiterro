738feb96f7ce2438b510104bb75da7ba42691161
ykk0425 <187173741+ykk0425@users.noreply.github.com>
2025-02-13 14:45:43 +0000

    [Add]ユーザーページ

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User < ApplicationRecord
       file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
       profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
-    profile_image.variant(resaixe_to_limit: [width, height]).processed
+    profile_image.variant(resize_to_limit: [width, height]).processed
   end
 
 end
